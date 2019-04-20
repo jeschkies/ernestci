b4aec1b1b4cca2da9c35406a021d83dc971f93f3
Karsten Jeschkies <k@jeschkies.xyz>
2019-04-20 19:47:44 +0200

    Use Strucopt to parse arguments.

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use]
+extern crate structopt;
+
+use structopt::StructOpt;
+
+/// Ernest CI Agent
+#[derive(StructOpt, Debug)]
+#[structopt(name = "agent")]
+struct Opt {
+    /// Github Commit to build.
+    #[structopt(name = "COMMIT")]
+    files: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
