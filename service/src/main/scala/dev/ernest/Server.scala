ea5e38373669f6c9b76ecce59190812be6973397
Karsten Jeschkies <k@jeschkies.xyz>
2019-04-26 13:03:04 +0200

    Serve agent from service.

--- service/src/main/scala/dev/ernest/Server.scala
+++ service/src/main/scala/dev/ernest/Server.scala
@@ -1,18 +1,22 @@
 package dev.ernest
 
+import java.nio.file.Paths
+
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.{HttpApp, Route}
-
-import scala.io.Source
 
 object Server extends HttpApp {
 
   override def routes: Route =
     path("hello") {
       get {
-        val resource = Source.fromResource("foo").getLines().mkString(",")
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Say $resource to akka-http</h1>"))
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Say hello to akka-http</h1>"))
       }
+    } ~
+    path("agent") {
+      val agent = Paths.get(System.getProperty("user.dir"), "../agent/target/debug/agent").toFile
+      require(agent.exists(), s"Agent does not exist in ${agent.getAbsolutePath}")
+      getFromFile(agent, ContentTypes.`application/octet-stream`)
     }
 
   def main(args: Array[String]): Unit = {
