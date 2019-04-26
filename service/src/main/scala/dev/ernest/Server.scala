1ba7b66e1bdb8a5ed7b9c58aaa2f5f38a9ef66a4
Karsten Jeschkies <k@jeschkies.xyz>
2019-04-26 12:36:20 +0200

    Assemble Agent with `:agent:assemble` task.

--- service/src/main/scala/dev/ernest/Server.scala
+++ service/src/main/scala/dev/ernest/Server.scala
@@ -3,12 +3,15 @@ package dev.ernest
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.{HttpApp, Route}
 
+import scala.io.Source
+
 object Server extends HttpApp {
 
   override def routes: Route =
     path("hello") {
       get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        val resource = Source.fromResource("foo").getLines().mkString(",")
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Say $resource to akka-http</h1>"))
       }
     }
 
